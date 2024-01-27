a4287d8f560d330a8707b4d94b5af367757fd28b
Chandan <hb7102849@gmail.com>
2024-01-28 00:55:59 +0530

    link

--- backend/controller/shop.js
+++ backend/controller/shop.js
@@ -39,7 +39,7 @@ router.post("/create-shop", catchAsyncErrors(async (req, res, next) => {
 
     const activationToken = createActivationToken(seller);
 
-    const activationUrl = `http://localhost:3000/seller/activation/${activationToken}`;
+    const activationUrl = `https://multi-vendor-chandan.vercel.app/seller/activation/${activationToken}`;
 
     try {
       await sendMail({
