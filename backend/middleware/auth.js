63deab9e24a021af3b515af6de452ef5e7092911
Chandan <hb7102849@gmail.com>
2024-01-27 23:30:14 +0530

    login issues

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -6,7 +6,7 @@ const Shop = require("../model/shop");
 
 exports.isAuthenticated = catchAsyncErrors(async(req,res,next) => {
     const {token} = req.cookies;
-
+    console.log(token)
     if(!token){
         return next(new ErrorHandler("Please login to continue", 401));
     }
