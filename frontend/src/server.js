526187cf90eee94e17a396f3f856816b8df97545
Chandan <hb7102849@gmail.com>
2024-01-27 12:26:38 +0530

    deploy

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "http://localhost:8000/api/v2";
+export const server = "https://multivendor-zk0e.onrender.com/api/v2";
 
-export const backend_url="http://localhost:8000"
+export const backend_url="https://multivendor-zk0e.onrender.com"
