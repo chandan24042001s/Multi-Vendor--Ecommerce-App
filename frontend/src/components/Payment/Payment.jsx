d0242d5194d2256924c95760ede4613996e7ec81
Chandan <hb7102849@gmail.com>
2024-01-27 15:23:37 +0530

    currency

--- frontend/src/components/Payment/Payment.jsx
+++ frontend/src/components/Payment/Payment.jsx
@@ -36,7 +36,7 @@ const Payment = () => {
           {
             description: "Sunflower",
             amount: {
-              currency_code: "USD",
+              currency_code: "inr",
               value: orderData?.totalPrice,
             },
           },
