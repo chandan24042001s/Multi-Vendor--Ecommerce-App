0b5b39a9ea7a27ff64397f4c22704066f9705943
Chandan <hb7102849@gmail.com>
2024-01-27 14:28:08 +0530

    activation

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -35,7 +35,7 @@ router.post("/create-user", async (req, res, next) => {
 
     const activationToken = createActivationToken(user);
 
-    const activationUrl = `http://localhost:3000/activation/${activationToken}`;
+    const activationUrl = `https://multi-vendor-chandan.vercel.app/activation/${activationToken}`;
 
     try {
       await sendMail({
