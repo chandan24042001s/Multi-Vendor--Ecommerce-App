584d3e10a3f302914cc38c796df139cb9694802d
Chandan <hb7102849@gmail.com>
2024-01-28 01:24:05 +0530

    console

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,7 +1,7 @@
 // create token and saving that in cookies
 const sendToken = (user, statusCode, res) => {
   const token = user.getJwtToken();
-
+  console.log(token)
   // Options for cookies
   const options = {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
